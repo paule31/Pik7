e7994017e816ea47f616e8a07d3691efd5f525ab
Peter Kröner <peter@peterkroener.de>
2013-02-25 11:46:00 +0100

    Now using grunt-parallel and templates for zip filename version number
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ qunit: {
 },
 
 requirejs: {
-  compile: {
+  all: {
     options: {
       baseUrl: 'src/script',
       paths: {
@@ -88,7 +88,7 @@ clean: [
 compress: {
   zip: {
     options: {
-      archive: 'pik7-' + JSON.parse(require('fs').readFileSync('package.json')).version + '.zip'
+      archive: 'pik7-<%= pkg.version %>.zip'
     },
     files: [
       { src: 'index.html' },
@@ -102,19 +102,34 @@ compress: {
       { src: 'presentations/Template/**' }
     ]
   }
+},
+
+parallel: {
+  compile: [
+    { grunt: true, args: ['coffee'] },
+    { grunt: true, args: ['stylus'] }
+  ],
+  testAndOptimize: [
+    { grunt: true, args: ['connect', 'qunit'] },
+    { grunt: true, args: ['requirejs'] }
+  ]
 }
 
 });
 
 
 grunt.loadNpmTasks('grunt-contrib');
+grunt.loadNpmTasks('grunt-parallel');
 
 
-grunt.registerTask('compile',   ['stylus', 'coffee', 'copy']);
-grunt.registerTask('test',      ['connect', 'qunit']);
+grunt.registerTask('compile', ['parallel:compile', 'copy']);
+grunt.registerTask('test',    ['connect', 'qunit']);
+grunt.registerTask('finish',  ['clean', 'compress']);
 
-grunt.registerTask('dev',       ['compile', 'test', 'requirejs']);
-grunt.registerTask('default',   ['compile', 'test', 'requirejs', 'clean', 'compress']);
+
+grunt.registerTask('dev-front', ['compile']);
+grunt.registerTask('dev',       ['compile', 'parallel:testAndOptimize']);
+grunt.registerTask('default',   ['compile', 'parallel:testAndOptimize', 'finish']);
 
 
 };
